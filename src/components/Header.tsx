a9b116c8008067bf990805b3b10ca514a3233a77
Baran Akıllı <akillibaran@hotmail.com>
2023-12-25 21:16:33 +0300

    pretty commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 
 class Header extends Component {
-  shouldComponentUpdate(nextProps: any, nextState: any) {
+      shouldComponentUpdate(nextProps: any, nextState: any) {
     return false;
   }
 
-  render() {
-    return <h1>RoboFriends</h1>;
+        render() {
+      return <h1>RoboFriends</h1>;
+
   }
 }
 export default Header;
